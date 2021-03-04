ae1ec9aef64103ac8cde8cb74b226b18be11dc36
Denis Klimenko <jaschik26@yandex.ru>
2021-03-04 10:00:35 +0400

    feat: implement min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,51 @@
 
 exports.min = function min (array) {
-  return 0;
+
+  let min = 0;
+
+  if (array === undefined) {
+    return min;
+  };
+
+  array.forEach(element => {
+    if (element <= min) {
+      min = element;
+    } 
+  });
+
+  return min
 }
 
 exports.max = function max (array) {
-  return 0;
+
+  let max = 0;
+  
+  if (array === undefined) {
+    return max;
+  };
+
+  array.forEach(element => {
+    if (element >= max) {
+      max = element;
+    }
+  });
+  return max
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+  let avg = 0
+  
+  if (array === undefined) {
+    return avg;
+  };
+
+  let sum = 0
+  array.forEach(element => {
+    sum += element
+  });
+  
+  avg = sum / array.length || 0;
+
+  return avg
 }
